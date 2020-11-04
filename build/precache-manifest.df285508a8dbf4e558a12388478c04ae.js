6710dd8bee913860b2e5f04858bb985341c6947b
Michelle Kang <kang.mic@husky.neu.edu>
2020-11-03 19:06:21 -0500

    deploying  to  heroku

---
+++ build/precache-manifest.df285508a8dbf4e558a12388478c04ae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b3a57ef0aea45d1c9da7a6f8acfeec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96b63d3c1ac832b55a85",
+    "url": "/static/css/2.42a5fdae.chunk.css"
+  },
+  {
+    "revision": "fef4c2bde946074e320c",
+    "url": "/static/css/main.1dc7bf28.chunk.css"
+  },
+  {
+    "revision": "96b63d3c1ac832b55a85",
+    "url": "/static/js/2.46389bc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46389bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef4c2bde946074e320c",
+    "url": "/static/js/main.29fadcb9.chunk.js"
+  },
+  {
+    "revision": "861d36af0fdb5a574f7b",
+    "url": "/static/js/runtime-main.b24ffb90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
